5c9750fb012b937fcab67000e292e5bf877dc4e8
Ava <avahom16@gmail.com>
2026-01-17 22:50:04 -0500

    Cleaner tile logic

---
+++ src/LoadoutLogic.py
@@ -0,0 +1,54 @@
+from typing import List, Optional
+
+def same_parity(n1: int, n2: int) -> bool:
+    return (n1 % 2 == 0) == (n2 % 2 == 0)
+
+class Loadout:
+    def __init__(self):
+        self._lineup: List[Optional[str]] = [None] * 10
+        self._longest_len = 0
+
+    def tile(self, unit) -> str:
+        total_spaces = self._longest_len
+        tile: str
+        if unit is not None:
+            unit_len = len(unit)
+            side_spaces = (total_spaces - unit_len) // 2
+            spaces = ' ' * side_spaces
+            extra_space = ''
+            if not same_parity(total_spaces, unit_len):
+                extra_space = ' '
+            tile = f'[{spaces}{unit}{spaces}{extra_space}]'
+        else:
+            spaces = ' ' * total_spaces
+            tile = f'[{spaces}]'
+        return tile
+
+    def __str__(self) -> str:
+        result = ''
+        for idx, unit in enumerate(self._lineup, start=1):
+            result += self.tile(unit) + ' '
+            if idx == 5:
+                result += '\n'
+        return result
+
+    @property
+    def lineup(self):
+        return self._lineup
+
+    def next_empty(self) -> int:
+        return self._lineup.index(None) + 1
+
+    def update_longest(self) -> None:
+        units = [u for u in self._lineup if u is not None]
+        new_longest = 0
+        if len(units) > 0:
+            new_longest = len(max(units, key=len))
+        if new_longest > self._longest_len:
+            self._longest_len = new_longest
+
+    def add(self, idx, unit: str):
+        if idx is None:
+            idx = self.next_empty()
+        self._lineup[idx - 1] = unit
+        self.update_longest()
