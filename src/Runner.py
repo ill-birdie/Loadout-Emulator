5c54b747290cff9491eb7be739a24704ecd38156
Ava <avahom16@gmail.com>
2026-01-17 23:42:59 -0500

    Runner's add() method handles invalid indices

--- src/Runner.py
+++ src/Runner.py
@@ -23,16 +23,15 @@ def execute(full_cmd: str) -> None:
             print(f'Unknown command: "{cmd}"')
 
 def add(args: List) -> None:
-    if not args[-1].isdigit():
-        unit_name = ' '.join(args)
-        loadout.add(None, unit_name)
-    else:
-        try:
-            idx = int(args[-1])
-            unit_name = ' '.join(args[:-1])
-            loadout.add(idx, unit_name)
-        except IndexError:
-            print('Missing required arguments')
+    try:
+        idx = int(args[-1])
+        if not 1 <= idx <= 10:
+            raise ValueError
+        unit = ' '.join(args[:-1])
+        loadout.add(idx, unit)
+    except ValueError:
+        unit = ' '.join(args)
+        loadout.add(None, unit)
 
 loadout = Loadout()
 run()
